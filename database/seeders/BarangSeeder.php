21b0cf767690f057570b532a0b871d4c0b0a13a8
Iqbal Firmansyah Adinda <134963689+IqbalFirmasnyah@users.noreply.github.com>
2023-11-24 14:15:07 +0700

    Laravel

---
+++ database/seeders/BarangSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Barang;
+
+class BarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Barang::factory()->count(3)->sequence(
+            ['nama'=> 'Apel'],
+            ['nama'=> 'Kelengkeng'],
+            ['nama'=> 'Durian'],
+        )->create();
+    }
+}
